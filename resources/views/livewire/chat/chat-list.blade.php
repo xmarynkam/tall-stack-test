7295fbadb7af1f5710eb8be2c2b6090204056b66
Alyona Yakymiv <alyona.yakymiv.work@gmail.com>
2025-02-18 10:54:01 +0200

    chat participant list

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -36,6 +36,83 @@
     </header>
 
     <main class="overflow-y-scroll overflow-hidden grow  h-full relative" style="contain:content">
+        <ul class="p-2 grid w-full spacey-y-2">
+            <li class="py-3 hover:bg-gray-50 rounded-2xl dark:hover:bg-gray-700/70 transition-colors duration-150 flex gap-4 relative w-full cursor-pointer px-2">
+                <a href="#" class="shrink-0">
+                    <x-avatar></x-avatar>
+                </a>
 
+                <aside class="grid grid-cols-12 w-full">
+                    <a href="#" class="col-span-11 border-b pb-2 border-gray-200 relative overflow-hidden truncate leading-5 w-full flex-nowrap p-1">
+                        <div class="flex justify-between w-full items-center">
+                            <h6 class="truncate font-medium tracking-wider text-gray-500">
+                                Alyona Yakymiv
+                            </h6>
+                            <small class="text-gray-700">5d</small>
+                        </div>
+                        <div class="flex gap-x-2 items-center">
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
+                                    <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0"/>
+                                    <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708"/>
+                                </svg>
+                            </span>
+{{--                            <span>--}}
+{{--                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">--}}
+{{--                                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>--}}
+{{--                                </svg>--}}
+{{--                            </span>--}}
+
+                            <p class="grow truncate text-sm font-[100]">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda autem corporis cum cumque deserunt, ducimus eligendi eum facere ipsum laborum libero magnam porro, quaerat qui repellendus repudiandae sapiente voluptatem.
+                            </p>
+
+                            <span class="font-bold p-px px-2 text-xs shrink-0 rounded-full bg-blue-500 text-white">5</span>
+                        </div>
+                    </a>
+
+                    <div class="col-span-1 flex flex-col text-center my-auto">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical w-6 h-6 text-gray-700" viewBox="0 0 16 16">
+                                        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                                    </svg>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <div class="w-full p-1">
+                                    <button
+                                        class="items-center gap-3 flex w-full px-4 py-2 text-left text-sm leading-5 text-gray-500 hover:bg-gray-100 transition-all duration-150 ease-in-out focus:outline-none focus:bg-gray-100"
+                                    >
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                                            </svg>
+                                        </span>
+
+                                        View Profile
+                                    </button>
+
+                                    <button
+                                        class="items-center gap-3 flex w-full px-4 py-2 text-left text-sm leading-5 text-gray-500 hover:bg-gray-100 transition-all duration-150 ease-in-out focus:outline-none focus:bg-gray-100"
+                                    >
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                                            </svg>
+                                        </span>
+
+                                        Delete
+                                    </button>
+                                </div>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                </aside>
+            </li>
+        </ul>
     </main>
 </div>
